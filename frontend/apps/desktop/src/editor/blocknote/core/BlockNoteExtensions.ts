535899613612ae362f89905a45e0d76da2b92853
iskaktoltay <iskaktoltay@gmail.com>
2024-07-17 12:32:14 +0500

    updated parsing of pasted markdown to account for heading hierarchy and lists

--- frontend/apps/desktop/src/editor/blocknote/core/BlockNoteExtensions.ts
+++ frontend/apps/desktop/src/editor/blocknote/core/BlockNoteExtensions.ts
@@ -28,7 +28,7 @@ import {BlockContainer, BlockGroup, Doc} from './extensions/Blocks'
 import {BlockNoteDOMAttributes} from './extensions/Blocks/api/blockTypes'
 import {CustomBlockSerializerExtension} from './extensions/Blocks/api/serialization'
 import blockStyles from './extensions/Blocks/nodes/Block.module.css'
-import {MarkdownExtension} from './extensions/Markdown/MarkdownExtension'
+import {createMarkdownExtension} from './extensions/Markdown/MarkdownExtension'
 import {Placeholder} from './extensions/Placeholder/PlaceholderExtension'
 import {TextAlignmentExtension} from './extensions/TextAlignment/TextAlignmentExtension'
 import {TextColorExtension} from './extensions/TextColor/TextColorExtension'
@@ -85,7 +85,8 @@ export const getBlockNoteExtensions = <BSchema extends HMBlockSchema>(opts: {
     Text,
 
     // copy paste:
-    MarkdownExtension,
+    // @ts-ignore
+    createMarkdownExtension(opts.editor),
 
     // block manupulations:
     BlockManipulationExtension,
