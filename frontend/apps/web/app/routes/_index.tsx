f48064f7345ecfc9689e57dc76f41fdfbd70784f
Eric Vicenti <eric@vicenti.net>
2024-08-13 02:16:04 +0200

    Home page

--- frontend/apps/web/app/routes/_index.tsx
+++ frontend/apps/web/app/routes/_index.tsx
@@ -1,38 +1,44 @@
-import {toPlainMessage} from "@bufbuild/protobuf";
 import type {MetaFunction} from "@remix-run/node";
+import {LoaderFunction} from "@remix-run/node";
 import {useLoaderData} from "@remix-run/react";
+import {HMDocument} from "@shm/shared";
 import {UIAvatar} from "@shm/ui/src/avatar";
 import {Button} from "@tamagui/button";
 import {Text} from "@tamagui/core";
 import {YStack} from "@tamagui/stacks";
+import {deserialize} from "superjson";
+import {getConfig} from "~/config";
+import {DocumentPage} from "~/document";
+import {loadHMDocument} from "~/loaders";
 import {queryClient} from "../client";
 
-export const meta: MetaFunction = () => {
+// Remove these if you want the error:
+queryClient;
+UIAvatar;
+Button;
+Text;
+YStack;
+// seriously, wtf
+
+export const meta: MetaFunction = ({data}) => {
+  const document: HMDocument = deserialize(data.document);
   return [
-    {title: "Seed Hypermedia"},
+    {title: document.metadata?.name || "Untitled"},
     // {name: "description", content: "Welcome to Remix!"},
   ];
 };
 
-export const loader = async () => {
-  const allEntities = await queryClient.entities.searchEntities({});
-  return {entities: toPlainMessage(allEntities).entities};
+export const loader: LoaderFunction = async ({params, request}) => {
+  const url = new URL(request.url);
+  const v = url.searchParams.get("v");
+  const {registeredAccountUid} = getConfig();
+  if (!registeredAccountUid) throw new Error("No registered account uid");
+  // todo, use version "v"
+  return await loadHMDocument(registeredAccountUid, []);
 };
 
-export default function Index() {
-  const l = useLoaderData<typeof loader>();
-
-  return (
-    <YStack>
-      <UIAvatar />
-      {l.entities.map((entity) => {
-        return (
-          <Text key={entity.id}>
-            {entity.title} - {entity.id}
-          </Text>
-        );
-      })}
-      <Button onPress={() => alert("pressed")}>Press m1e</Button>
-    </YStack>
-  );
+export default function SiteDocument() {
+  const data = useLoaderData<typeof loader>();
+  const document: HMDocument = deserialize(data.document);
+  return <DocumentPage document={document} />;
 }
