b7eb398d1380be1fa9892ddd719a80a11495dea9
Horacio Herrera <hi@horacioh.com>
2024-08-24 00:35:52 +0200

    horacio: add react adapter

--- frontend/apps/web2/astro.config.mjs
+++ frontend/apps/web2/astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import node from "@astrojs/node";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [tailwind()],
+  integrations: [tailwind(), react()],
   adapter: node({
     mode: "standalone"
   })
