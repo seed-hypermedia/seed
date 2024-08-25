d3cb5d62b65b5dae925d2a8a8c084107f60e7aa4
Horacio Herrera <hi@horacioh.com>
2024-08-25 09:34:04 +0200

    horacio: remove home document from list

--- frontend/apps/web2/src/list-documents.ts
+++ frontend/apps/web2/src/list-documents.ts
@@ -2,5 +2,5 @@ import {queryClient} from "./client";
 
 export async function listDocuments(account: string) {
   let docs = await queryClient.documents.listDocuments({account});
-  return docs.documents;
+  return docs.documents.filter((item) => item.path != "");
 }
