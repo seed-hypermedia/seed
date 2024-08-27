5f9cfa053d932fb9f65dd9695efe7a38047b7130
Horacio Herrera <hi@horacioh.com>
2024-08-28 00:40:36 +0200

    horacio: wip

--- frontend/apps/web2/src/middleware/home-document.ts
+++ frontend/apps/web2/src/middleware/home-document.ts
@@ -2,9 +2,10 @@ import {defineMiddleware} from "astro:middleware";
 
 export const onRequest = defineMiddleware(
   async function homeDocMiddleware(context, next) {
+    // context.locals.accountUid =
+    //   "z6Mkvz9TgGtv9zsGsdrksfNk1ajbFancgHREJEz3Y2HsAVdk";
     context.locals.accountUid =
-      "z6Mkvz9TgGtv9zsGsdrksfNk1ajbFancgHREJEz3Y2HsAVdk";
-    // context.locals.accountUid = "z6MkkEnUheepjpmhwkF7m8tVLPXAzBadPeajriaVUXYoTteJ";
+      "z6MkkEnUheepjpmhwkF7m8tVLPXAzBadPeajriaVUXYoTteJ";
     // return a Response or the result of calling `next()`
     return next();
   }
