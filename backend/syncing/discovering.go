802b384e25e8b38c269203965b1ba1105db8487c
juligasa <11684004+juligasa@users.noreply.github.com>
2024-09-11 13:31:22 +0200

    chore(daemon) increase the discovery to for local peer syncs

--- backend/syncing/discovering.go
+++ backend/syncing/discovering.go
@@ -19,8 +19,8 @@ import (
 // DefaultDiscoveryTimeout is how long do we wait to discover a peer and sync with it
 const (
 	DefaultDiscoveryTimeout = time.Second * 30
-	DefaultSyncingTimeout   = 1 * DefaultDiscoveryTimeout / 6
-	DefaultDHTTimeout       = 5 * DefaultDiscoveryTimeout / 6
+	DefaultSyncingTimeout   = 1 * DefaultDiscoveryTimeout / 5
+	DefaultDHTTimeout       = 4 * DefaultDiscoveryTimeout / 5
 )
 
 // DiscoverObject discovers an object in the network. If not found, then it returns an error
