6d8f1bc96121ca9dab4f692650b8d0f47c77f93c
Horacio Herrera <hi@horacioh.com>
2024-09-19 15:54:59 +0200

    not found page

--- frontend/apps/web/app/not-found.tsx
+++ frontend/apps/web/app/not-found.tsx
@@ -1,4 +1,27 @@
+import {YStack} from "@tamagui/stacks";
 import {SizableText} from "@tamagui/text";
-export function NotFoundPage() {
-  return <SizableText>Document Not Found</SizableText>;
+import {SiteDocumentPayload} from "./loaders";
+import {Container} from "./ui/container";
+export function NotFoundPage({homeMetadata, homeId}: SiteDocumentPayload) {
+  return (
+    <YStack>
+      <Container>
+        <YStack
+          alignSelf="center"
+          width={600}
+          gap="$5"
+          borderWidth={1}
+          borderColor="$color8"
+          borderRadius="$4"
+          padding="$5"
+          elevation="$4"
+        >
+          <SizableText size="$8" fontWeight="bold">
+            Document Not Found
+          </SizableText>
+          <SizableText>This document is not on this server (yet).</SizableText>
+        </YStack>
+      </Container>
+    </YStack>
+  );
 }
