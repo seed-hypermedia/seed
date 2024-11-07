bc13800a6575079aa294f62df6aef6daa4ecbf43
iskaktoltay <iskaktoltay@gmail.com>
2024-11-07 12:59:18 +0500

    button block progress WIP

---
+++ frontend/apps/desktop/src/editor/launcher-item.tsx
@@ -0,0 +1,45 @@
+import {Button, SizableText, XStack} from '@shm/ui'
+import {useLayoutEffect, useRef} from 'react'
+import {SwitcherItem} from './editor-types'
+
+export function LauncherItem({
+  item,
+  selected = false,
+  onFocus,
+  onMouseEnter,
+}: {
+  item: SwitcherItem
+  selected: boolean
+  onFocus: any
+  onMouseEnter: any
+}) {
+  const elm = useRef<HTMLDivElement>(null)
+
+  useLayoutEffect(() => {
+    if (selected) {
+      elm.current?.scrollIntoView({block: 'nearest'})
+    }
+  }, [selected])
+
+  return (
+    <Button
+      ref={elm}
+      key={item.key}
+      onPress={() => {
+        item.onSelect()
+      }}
+      backgroundColor={selected ? '$brand4' : undefined}
+      hoverStyle={{
+        backgroundColor: selected ? '$brand4' : undefined,
+      }}
+      onFocus={onFocus}
+      onMouseEnter={onMouseEnter}
+    >
+      <XStack f={1} justifyContent="space-between">
+        <SizableText numberOfLines={1}>{item.title}</SizableText>
+
+        <SizableText color="$color10">{item.subtitle}</SizableText>
+      </XStack>
+    </Button>
+  )
+}
