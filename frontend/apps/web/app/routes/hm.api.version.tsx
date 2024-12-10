5b4d711d8fd64d9803532f9db99486ead0055274
Eric Vicenti <eric@vicenti.net>
2024-12-09 22:04:26 -0800

    consistent docker releases, web version API

--- frontend/apps/web/app/routes/hm.api.version.tsx
+++ frontend/apps/web/app/routes/hm.api.version.tsx
@@ -1,11 +1,33 @@
 import type {LoaderFunction} from "@remix-run/node";
 import {json} from "@remix-run/node";
+import {DAEMON_HTTP_URL} from "@shm/shared";
 import fs from "fs/promises";
 
 async function getVersionInfo() {
-  const version = await fs.readFile("VERSION", "utf8");
-  const branch = await fs.readFile("BRANCH", "utf8");
-  return {version, branch};
+  let commit: string | null = null;
+  let branch: string | null = null;
+  let date: string | null = null;
+  try {
+    commit = await fs.readFile("COMMIT_HASH", "utf8");
+  } catch (e) {
+    console.error("Failed to read COMMIT_HASH file", e);
+  }
+  try {
+    branch = await fs.readFile("BRANCH", "utf8");
+  } catch (e) {
+    console.error("Failed to read BRANCH file", e);
+  }
+  try {
+    date = await fs.readFile("DATE", "utf8");
+  } catch (e) {
+    console.error("Failed to read DATE file", e);
+  }
+  return {commit, branch, date};
+}
+
+async function getDaemonVersionInfo() {
+  const version = await fetch(`${DAEMON_HTTP_URL}/debug/version`);
+  return await version.json();
 }
 
 let versionInfo: Awaited<ReturnType<typeof getVersionInfo>> | null = null;
@@ -14,5 +36,5 @@ export const loader: LoaderFunction = async () => {
   if (!versionInfo) {
     versionInfo = await getVersionInfo();
   }
-  return json(versionInfo);
+  return json({web: versionInfo, daemon: await getDaemonVersionInfo()});
 };
