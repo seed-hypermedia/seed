2d1fb1323932bb0e0a2986f548f25055394a1a9b
Eric Vicenti <eric@vicenti.net>
2025-02-05 17:29:09 +0100

    List service config admin API endpoint

---
+++ frontend/apps/web/app/routes/hm.api.admin.list-config.tsx
@@ -0,0 +1,25 @@
+import {ActionFunction, json} from "@remix-run/node";
+import {z} from "zod";
+import {adminSecret, getServiceConfig} from "~/site-config";
+
+const listConfigSchema = z
+  .object({
+    adminSecret: z.string(),
+  })
+  .strict();
+
+export const action: ActionFunction = async ({request}) => {
+  if (request.method !== "POST") {
+    return json({message: "Method not allowed"}, {status: 405});
+  }
+  const data = await request.json();
+  const payload = listConfigSchema.parse(data);
+  if (payload.adminSecret !== adminSecret || !adminSecret) {
+    return json({message: "Invalid admin secret"}, {status: 401});
+  }
+  const serviceConfig = await getServiceConfig();
+  if (!serviceConfig) {
+    return json({message: "Service config not found"}, {status: 404});
+  }
+  return json(serviceConfig);
+};
