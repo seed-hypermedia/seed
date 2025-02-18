85dfb3af168e30e7d4b4b092454825371f8f4332
Horacio Herrera <hi@horacioh.com>
2025-02-18 13:46:15 +0100

    update explanation on stubs

--- frontend/packages/editor/src/link-utils.ts
+++ frontend/packages/editor/src/link-utils.ts
@@ -22,14 +22,7 @@ export async function resolveHypermediaUrl(url: string) {
    * TODO ERIC
    * https://github.com/seed-hypermedia/seed/blob/509cc0e68e813d4b64b586a57bd256a5787b88e3/frontend/apps/desktop/src/models/web-links.ts#L93
    *
-   * should return the metatags of the html page or null if the url is not a hypermedia page
-   * example: {
-   *  hypermedia_id: '123',
-   *  hypermedia_version: '1.0',
-   *  hypermedia_title: 'My Page',
-   *  hypermedia_description: 'This is a description',
-   *  hypermedia_image: 'ipfs://Qm123',
-   *  hypermedia_url: 'https://hyper.media/hm/123',
-   * }
+   * should return the UnpackedHypermediaId (partial) or null if the url is not a hypermedia page
+   
    */
 }
