1a2f36af03548e344fe0cbbe3c911587f7c70d52
Eric Vicenti <eric@vicenti.net>
2025-02-20 15:41:02 +0100

    Dev experiment to see protocol of origin request

--- frontend/apps/web/app/request.ts
+++ frontend/apps/web/app/request.ts
@@ -1,8 +1,11 @@
 export function parseRequest(request: Request) {
   const url = new URL(request.url);
   const forwardedHost = request.headers.get("x-forwarded-host");
+  const forwardedProto = request.headers.get("x-forwarded-proto");
   console.log("x-forwarded-host", forwardedHost);
+  console.log("x-forwarded-proto", forwardedProto);
   console.log("url.hostname", url.hostname);
+  console.log("url.protocol", url.protocol);
   const hostname = forwardedHost || url.hostname;
   let pathParts = url.pathname.split("/").slice(1);
   if (pathParts.at(-1) === "") {
