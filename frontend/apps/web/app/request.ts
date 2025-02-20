5052e1f7f5162e933f5cd5e8e8a5b491ac159b43
Eric Vicenti <eric@vicenti.net>
2025-02-20 13:06:32 +0100

    Logging for web URL nightmare

--- frontend/apps/web/app/request.ts
+++ frontend/apps/web/app/request.ts
@@ -1,12 +1,16 @@
 export function parseRequest(request: Request) {
   const url = new URL(request.url);
-  const hostname = request.headers.get("x-forwarded-host") || url.hostname;
+  const forwardedHost = request.headers.get("x-forwarded-host");
+  console.log("x-forwarded-host", forwardedHost);
+  console.log("url.hostname", url.hostname);
+  const hostname = forwardedHost || url.hostname;
   let pathParts = url.pathname.split("/").slice(1);
   if (pathParts.at(-1) === "") {
     pathParts = pathParts.slice(0, -1);
   }
   return {
     hostname,
+    fullOrigin: `https://${hostname}`, // Where do we get the protocol from?
     url,
     pathParts,
     method: request.method,
