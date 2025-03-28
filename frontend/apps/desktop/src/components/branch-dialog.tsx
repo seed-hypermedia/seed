08a0fbf89114bba1872cb37ae557eb6ba463408a
Eric Vicenti <eric@vicenti.net>
2025-03-28 16:20:07 +0100

    Branch dialog location selection improvements

--- frontend/apps/desktop/src/components/branch-dialog.tsx
+++ frontend/apps/desktop/src/components/branch-dialog.tsx
@@ -1,14 +1,21 @@
 import {useMyAccountIds} from '@/models/daemon'
 import {useForkDocument, useListDirectory} from '@/models/documents'
+import {useGatewayUrl} from '@/models/gateway-settings'
 import {trpc} from '@/trpc'
 import {pathNameify} from '@/utils/path'
 import {useNavigate} from '@/utils/useNavigate'
-import {hmId, UnpackedHypermediaId} from '@shm/shared'
+import {
+  createSiteUrl,
+  createWebHMUrl,
+  hmId,
+  UnpackedHypermediaId,
+} from '@shm/shared'
 import {useEntities, useEntity} from '@shm/shared/models/entity'
 import {Button} from '@shm/ui/button'
 import {Field} from '@shm/ui/form-fields'
 import {HMIcon} from '@shm/ui/hm-icon'
 import {SelectDropdown} from '@shm/ui/select-dropdown'
+import {ArrowUp} from '@tamagui/lucide-icons'
 import {useEffect, useMemo, useState} from 'react'
 import {Input, ScrollView, SizableText, Spinner, XStack, YStack} from 'tamagui'
 import {DialogTitle} from './dialog'
@@ -29,7 +36,7 @@ export function BranchDialog({
   if (!entity) return <Spinner />
   return (
     <YStack>
-      <DialogTitle>Fork "{entity?.document?.metadata.name}"</DialogTitle>
+      <DialogTitle>Branch from "{entity?.document?.metadata.name}"</DialogTitle>
       {entity ? (
         <>
           <NewLocationSelectionForm
@@ -56,7 +63,7 @@ export function BranchDialog({
                     })
                 }}
               >
-                Fork
+                Create Document Branch
               </Button>
             ) : null}
           </XStack>
@@ -100,12 +107,12 @@ function NewLocationSelectionForm({
     }
   }, [account, defaultAccountId])
   const parentId = useParentId(location)
-  const {data: directory} = useListDirectory(parentId)
+  const {data: directory} = useListDirectory(parentId, {mode: 'Children'})
   console.log({directory})
   return (
-    <YStack gap="$3">
+    <YStack gap="$3" marginVertical="$4">
       {/* <SizableText>{JSON.stringify(directory)}</SizableText> */}
-      <Field label="Branch Author Account" id="account">
+      <Field label="Author Account" id="account">
         {account && (
           <SelectDropdown
             value={account}
@@ -130,25 +137,62 @@ function NewLocationSelectionForm({
           />
         )}
       </Field>
-      <Field label="Location" id="location">
-        <ScrollView
-          minHeight={180}
-          borderWidth={1}
-          borderColor="$borderColor"
-          borderRadius="$4"
-        >
-          <YStack>
-            {directory?.map((d) => {
-              return <SizableText>{d.metadata.name}</SizableText>
-            })}
-          </YStack>
-        </ScrollView>
-      </Field>
+      {location ? (
+        <Field label="Location" id="location">
+          <XStack jc="space-between" ai="center">
+            {parentId ? (
+              <LocationPreview location={parentId} setLocation={setLocation} />
+            ) : null}
+            {(location.path?.length || 0) > 1 ? (
+              <Button
+                onPress={() => {
+                  setLocation(
+                    hmId('d', location.uid, {
+                      path: location.path?.slice(0, -1),
+                    }),
+                  )
+                }}
+                icon={ArrowUp}
+                size="$2"
+              />
+            ) : null}
+          </XStack>
+          <ScrollView
+            height={200}
+            borderWidth={1}
+            borderColor="$borderColor"
+            borderRadius="$4"
+          >
+            <YStack>
+              {directory?.map((d, index) => {
+                return (
+                  <Button
+                    onPress={() => {
+                      setLocation(
+                        hmId('d', location.uid, {
+                          path: [...d.path, location.path?.at(-1) || ''],
+                        }),
+                      )
+                    }}
+                  >
+                    <XStack jc="space-between" f={1}>
+                      <SizableText>{d.metadata.name}</SizableText>
+                      <SizableText color="$color8">
+                        {d.path?.at(-1)}
+                      </SizableText>
+                    </XStack>
+                  </Button>
+                )
+              })}
+            </YStack>
+          </ScrollView>
+        </Field>
+      ) : null}
       <Field label="URL Path" id="url-path">
         <Input
           id="url-path"
           value={location?.path?.at(-1) || ''}
-          onChangeText={(text) => {
+          onChangeText={(text: string) => {
             if (!location) return
             setLocation(
               hmId('d', location?.uid, {
@@ -163,12 +207,95 @@ function NewLocationSelectionForm({
   )
 }
 
-function URLPreview({location}: {location: UnpackedHypermediaId}) {
-  // todo render the final URL. base on the gateway URL and the sites homeDocument siteURL
+function LocationPreview({
+  location,
+  setLocation,
+}: {
+  location: UnpackedHypermediaId
+  setLocation: (location: UnpackedHypermediaId) => void
+}) {
+  const siteId = hmId('d', location.uid, {latest: true})
+  const site = useEntity(siteId)
+  const locationBreadcrumbIds = useMemo(() => {
+    if (!location) return []
+    return (
+      location.path
+        // ?.slice(0,)
+        ?.map((_path, index) =>
+          hmId('d', location.uid, {path: location.path?.slice(0, index + 1)}),
+        ) || []
+    )
+  }, [location.uid, location.path])
+  const newPathTerm = location.path?.at(-1) || ''
+  console.log('~~ ', locationBreadcrumbIds)
+  const locationBreadcrumbs = useEntities(locationBreadcrumbIds)
   return (
-    <SizableText color="$color8" size="$2" marginVertical="$4">
-      {location.id}
-    </SizableText>
+    <XStack paddingVertical="$2" ai="center" gap="$3">
+      <HMIcon id={siteId} metadata={site?.data?.document?.metadata} />
+      <SizableText
+        fontWeight="bold"
+        hoverStyle={{
+          textDecorationLine: 'underline',
+        }}
+        onPress={() => {
+          setLocation(hmId('d', location.uid))
+        }}
+      >
+        {site?.data?.document?.metadata.name}
+      </SizableText>
+      {locationBreadcrumbs.map((b, index) => {
+        return (
+          <SizableText
+            hoverStyle={{
+              textDecorationLine: 'underline',
+            }}
+            onPress={() => {
+              const path = b.data?.id?.path || []
+              setLocation(
+                hmId('d', location?.uid, {
+                  path: [...path, newPathTerm],
+                }),
+              )
+            }}
+          >
+            {b.data?.document?.metadata.name}
+          </SizableText>
+        )
+      })}
+      {/* <SizableText>{location.path?.at(-1) || ''}</SizableText> */}
+    </XStack>
+  )
+}
+
+function useDocumentUrl(location: UnpackedHypermediaId) {
+  const gatewayUrl = useGatewayUrl()
+  const {data: site} = useEntity(hmId('d', location.uid, {latest: true}))
+  if (!site || !gatewayUrl.data) return null
+  const siteUrl = site.document?.metadata.siteUrl
+  if (siteUrl) {
+    return createSiteUrl({
+      path: location.path,
+      hostname: siteUrl,
+    })
+  }
+  const url = createWebHMUrl(location.type, location.uid, {
+    path: location.path,
+    hostname: gatewayUrl.data,
+  })
+  return url
+}
+
+function URLPreview({location}: {location: UnpackedHypermediaId}) {
+  const url = useDocumentUrl(location)
+  return (
+    <YStack>
+      <SizableText color="$color10" size="$2">
+        Branch Destination URL
+      </SizableText>
+      <SizableText color="$blue11" size="$3">
+        {url}
+      </SizableText>
+    </YStack>
   )
 }
 
