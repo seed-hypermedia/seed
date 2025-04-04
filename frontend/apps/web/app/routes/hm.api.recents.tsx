ad406e520d94010b26843d2a057ef2b72a98ecb0
iskaktoltay <iskaktoltay@gmail.com>
2025-04-04 11:32:12 +0500

    added updated launcher design

---
+++ frontend/apps/web/app/routes/hm.api.recents.tsx
@@ -0,0 +1,26 @@
+// import {queryClient} from '@/client'
+// import {wrapJSON} from '@/wrapping'
+// import {SearchPayload, unpackHmId} from '@shm/shared'
+
+// export const loader = async ({request}: {request: Request}) => {
+//   const url = new URL(request.url)
+//   const searchQuery = url.searchParams.get('q') || ''
+//   const result = await queryClient.entities.searchEntities({
+//     query: searchQuery,
+//   })
+
+//   return wrapJSON<SearchPayload>({
+//     searchQuery,
+//     entities: result.entities
+//       .map((entity) => {
+//         const id = unpackHmId(entity.id)
+//         return (
+//           id && {
+//             id,
+//             title: entity.title,
+//           }
+//         )
+//       })
+//       .filter((result) => !!result),
+//   })
+// }
