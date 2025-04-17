a5e0dcc960fcee5749228c029222d0583f830e1b
juligasa <11684004+juligasa@users.noreply.github.com>
2025-04-17 07:52:03 +0200

    wip(daemon): Initial FT% installation Check

--- backend/daemon/sqlite_queries_test.go
+++ backend/daemon/sqlite_queries_test.go
@@ -3,6 +3,8 @@ package daemon
 import (
 	"seed/backend/storage"
 	"seed/backend/util/dqb"
+	"seed/backend/util/sqlite"
+	"seed/backend/util/sqlite/sqlitex"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -20,4 +22,34 @@ func TestDBQueries(t *testing.T) {
 	defer db.Close()
 	require.NoError(t, storage.InitSQLiteSchema(db))
 	require.NoError(t, dqb.GlobalQueries.Test(db))
+}
+
+func TestFTS(t *testing.T) {
+	t.Parallel()
+
+	db, err := storage.OpenSQLite("file::memory:?mode=memory", 0, 1)
+	require.NoError(t, err)
+	defer db.Close()
+
+	// Execute the CREATE VIRTUAL TABLE statement
+	err = db.WithTx(t.Context(), func(conn *sqlite.Conn) error {
+		return sqlitex.Exec(conn, `CREATE VIRTUAL TABLE ft USING fts5(characters);`, nil)
+	})
+	require.NoError(t, err)
+
+	// Execute the INSERT statement
+	err = db.WithTx(t.Context(), func(conn *sqlite.Conn) error {
+		return sqlitex.Exec(conn, `INSERT INTO ft(rowid, characters) VALUES(1, 'A B C D x x x E F x');`, nil)
+	})
+	require.NoError(t, err)
+
+	var match string
+	err = db.WithSave(t.Context(), func(conn *sqlite.Conn) error {
+		return sqlitex.Exec(conn, `SELECT * FROM ft WHERE characters MATCH 'NEAR(e d, 4)';`, func(stmt *sqlite.Stmt) error {
+			match = stmt.ColumnText(0)
+			return nil
+		})
+	})
+	require.NoError(t, err)
+	require.Equal(t, "A B C D x x x E F x", match)
 }
