3ab73b822d64915389a96eb833d10c8043f716c5
Eric Vicenti <eric@vicenti.net>
2025-04-21 11:35:44 +0200

    API progress

--- frontend/apps/web/app/routes/api.changes.$.tsx
+++ frontend/apps/web/app/routes/api.changes.$.tsx
@@ -3,10 +3,17 @@ import {apiGetter} from '@/server-api'
 import {BIG_INT, hmIdPathToEntityQueryPath} from '@shm/shared'
 
 export const loader = apiGetter(async (req) => {
-  const [_api, _document, uid, ...restPath] = req.pathParts
+  const [_api, _changes, uid, ...restPath] = req.pathParts
+  const path = hmIdPathToEntityQueryPath(restPath)
+  const latestDoc = await queryClient.documents.getDocument({
+    account: uid,
+    path,
+    version: undefined,
+  })
   const result = await queryClient.documents.listDocumentChanges({
     account: uid,
-    path: hmIdPathToEntityQueryPath(restPath),
+    path,
+    version: latestDoc.version,
     pageSize: BIG_INT,
   })
   return result.toJson()
--- frontend/apps/web/app/routes/api.citations.$.tsx
+++ frontend/apps/web/app/routes/api.citations.$.tsx
@@ -1,16 +1,17 @@
 import {queryClient} from '@/client'
 import {apiGetter} from '@/server-api'
-import {BIG_INT, hmId, packHmId} from '@shm/shared'
+import {BIG_INT, hmId, HMIDTypeSchema, packHmId} from '@shm/shared'
 
-export const loader = apiGetter(async (req) => {
-  const [_api, _document, uid, ...restPath] = req.pathParts
+export const loader = apiGetter(async ({pathParts}) => {
+  const [_api, _citations, type, uid, ...restPath] = pathParts
+  const t = HMIDTypeSchema.parse(type)
   const result = await queryClient.entities.listEntityMentions({
     id: packHmId(
-      hmId('d', uid, {
+      hmId(t, uid, {
         path: restPath,
       }),
     ),
     pageSize: BIG_INT,
   })
-  return result.toJson()
+  return {citations: result.mentions.map((m) => m.toJson())}
 })
