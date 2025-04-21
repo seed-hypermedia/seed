abb0b5a543b3eeb19553141cb96330f2ca057327
Eric Vicenti <eric@vicenti.net>
2025-04-21 11:51:47 +0200

    Fix entity API for comments

--- frontend/apps/web/app/routes/api.entity.$.tsx
+++ frontend/apps/web/app/routes/api.entity.$.tsx
@@ -5,12 +5,18 @@ import {hmIdPathToEntityQueryPath} from '@shm/shared'
 export const loader = apiGetter(async (req) => {
   const pathParts = req.pathParts
   const [_api, _document, type, uid, ...restPath] = pathParts
-  if (type !== 'd') {
-    throw new Error('Invalid entity type, document only')
+  if (type === 'd') {
+    const doc = await queryClient.documents.getDocument({
+      account: uid,
+      path: hmIdPathToEntityQueryPath(restPath),
+    })
+    return doc.toJson()
   }
-  const doc = await queryClient.documents.getDocument({
-    account: uid,
-    path: hmIdPathToEntityQueryPath(restPath),
-  })
-  return doc.toJson()
+  if (type === 'c') {
+    const comment = await queryClient.comments.getComment({
+      id: uid,
+    })
+    return comment.toJson()
+  }
+  throw new Error('Invalid entity type, document only')
 })
