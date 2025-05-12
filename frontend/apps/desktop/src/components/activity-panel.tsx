2c1e3056c17c580a1c7927d5389cf97fbc90fffb
Eric Vicenti <eric@vicenti.net>
2025-05-12 18:13:23 +0200

    Activity panel scrolls

--- frontend/apps/desktop/src/components/activity-panel.tsx
+++ frontend/apps/desktop/src/components/activity-panel.tsx
@@ -1,6 +1,6 @@
 import {UnpackedHypermediaId} from '@shm/shared/hm-types'
 import {DocumentAccessory} from '@shm/shared/routes'
-import {View} from 'tamagui'
+import {ScrollView, View} from 'tamagui'
 import {ActivityList} from './document-activity'
 
 export function ActivityPanel({
@@ -11,18 +11,20 @@ export function ActivityPanel({
   onAccessory: (accessory: DocumentAccessory) => void
 }) {
   return (
-    <View paddingHorizontal="$2">
-      <ActivityList
-        docId={docId}
-        onCommentFocus={(commentId, isReplying) => {
-          onAccessory({
-            key: 'discussions',
-            openComment: commentId,
-            openBlockId: undefined,
-            isReplying,
-          })
-        }}
-      />
-    </View>
+    <ScrollView>
+      <View paddingHorizontal="$2">
+        <ActivityList
+          docId={docId}
+          onCommentFocus={(commentId, isReplying) => {
+            onAccessory({
+              key: 'discussions',
+              openComment: commentId,
+              openBlockId: undefined,
+              isReplying,
+            })
+          }}
+        />
+      </View>
+    </ScrollView>
   )
 }
