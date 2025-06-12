ba98552a311fce69d05c30fa700601ff2a50d64f
iskaktoltay <iskaktoltay@gmail.com>
2025-06-12 15:36:02 +0500

    added support for buttons, twitter embeds and strikethrough annotation in emails

--- frontend/apps/emails/render.tsx
+++ frontend/apps/emails/render.tsx
@@ -1,6 +1,12 @@
+import fs from 'fs'
 import {sendNotificationsEmail} from './sender'
+
+const {email, opts, notifications} = JSON.parse(
+  fs.readFileSync('./mock-notification2.json', 'utf-8'),
+)
+
 ;(async () => {
-  sendNotificationsEmail()
+  sendNotificationsEmail(email, opts, notifications)
 
   console.log('✅ Test email sent successfully')
 })()
