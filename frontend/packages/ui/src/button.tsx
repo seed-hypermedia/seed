66f2cc6957b6d2d96a6a02aff6af9f73ba5711c2
Horacio Herrera <hi@horacioh.com>
2025-07-02 17:23:25 +0200

    updates ui components

--- frontend/packages/ui/src/button.tsx
+++ frontend/packages/ui/src/button.tsx
@@ -40,7 +40,7 @@ export const buttonVariants = cva(
         destructive:
           'bg-destructive/75 text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60',
         outline:
-          'border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50',
+          'border bg-transparent shadow-xs hover:bg-background hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50',
         secondary:
           'bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80',
         ghost:
