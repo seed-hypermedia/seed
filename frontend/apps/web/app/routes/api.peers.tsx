0bde9428530d58b67980719d94432dd2c82a4fb6
Eric Vicenti <eric@vicenti.net>
2025-07-10 16:22:34 +0200

    Peer listing endpoint

---
+++ frontend/apps/web/app/routes/api.peers.tsx
@@ -0,0 +1,23 @@
+import {queryClient} from '@/client'
+import {apiGetter} from '@/server-api'
+import {BIG_INT, ConnectionStatus} from '@shm/shared'
+
+const ConnectionStatusKeys = {
+  [ConnectionStatus.NOT_CONNECTED]: 'NOT_CONNECTED',
+  [ConnectionStatus.CONNECTED]: 'CONNECTED',
+  [ConnectionStatus.CAN_CONNECT]: 'CAN_CONNECT',
+  [ConnectionStatus.CANNOT_CONNECT]: 'CANNOT_CONNECT',
+  [ConnectionStatus.LIMITED]: 'LIMITED',
+} as const
+
+export const loader = apiGetter(async (req) => {
+  const peerList = await queryClient.networking.listPeers({
+    pageSize: BIG_INT,
+  })
+  return {
+    peers: peerList.peers.map((peer) => ({
+      id: peer.id,
+      status: ConnectionStatusKeys[peer.connectionStatus],
+    })),
+  }
+})
