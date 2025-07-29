0397edeeaf167b56bb8842c5806e960737381149
iskaktoltay <iskaktoltay@gmail.com>
2025-07-29 17:52:59 +0500

    added delete draft button to drafts page

--- frontend/apps/desktop/src/pages/drafts.tsx
+++ frontend/apps/desktop/src/pages/drafts.tsx
@@ -1,5 +1,7 @@
+import {useDeleteDraftDialog} from '@/components/delete-draft-dialog'
 import {MainWrapper} from '@/components/main-wrapper'
 import {useDraftList} from '@/models/documents'
+import {useNavigationDispatch} from '@/utils/navigation'
 import {useNavigate} from '@/utils/useNavigate'
 import {
   formattedDateMedium,
@@ -14,7 +16,8 @@ import {useResources} from '@shm/shared/models/entity'
 import {Button} from '@shm/ui/button'
 import {Container, PanelContainer} from '@shm/ui/container'
 import {SizableText} from '@shm/ui/text'
-import {useMemo} from 'react'
+import {Trash} from 'lucide-react'
+import React, {useMemo} from 'react'
 
 export default function DraftsPage() {
   const drafts = useDraftList()
@@ -89,57 +92,80 @@ export function DraftItem({
   breadcrumbs: HMMetadataPayload[]
 }) {
   const navigate = useNavigate()
+  const deleteDialog = useDeleteDraftDialog()
+  const dispatch = useNavigationDispatch()
   const metadata = item?.metadata
+
   return (
-    <Button
-      className="h-auto"
+    <div
+      className="group hover:bg-muted h-auto w-full cursor-pointer rounded px-4 py-2"
       onClick={() => {
         navigate({key: 'draft', id: item.id, accessory: {key: 'options'}})
       }}
     >
-      <div className="flex-1 items-center gap-4 overflow-hidden">
-        <div className="flex items-center gap-1 overflow-hidden">
-          {breadcrumbs.map((breadcrumb, idx) => (
-            <>
-              <Button
-                variant="link"
-                size="xs"
-                className="p-0"
-                onClick={(e) => {
-                  e.preventDefault()
-                  e.stopPropagation()
-                  navigate({
-                    key: 'document',
-                    id: breadcrumb.id,
-                  })
-                }}
-              >
-                {breadcrumb.metadata?.name ??
-                  breadcrumb.id?.path?.at(-1) ??
-                  '?'}
-              </Button>
-              {idx === breadcrumbs.length - 1 ? null : (
-                <SizableText size="xs" color="muted">
-                  /
-                </SizableText>
-              )}
-            </>
-          ))}
+      <div className="flex w-full items-center justify-between gap-4 overflow-hidden">
+        <div className="flex-1 overflow-hidden">
+          <div className="flex items-center gap-1 overflow-hidden">
+            {breadcrumbs.map((breadcrumb, idx) => (
+              <React.Fragment key={breadcrumb.id.uid || idx}>
+                <Button
+                  variant="link"
+                  size="xs"
+                  className="p-0"
+                  onClick={(e) => {
+                    e.preventDefault()
+                    e.stopPropagation()
+                    navigate({
+                      key: 'document',
+                      id: breadcrumb.id,
+                    })
+                  }}
+                >
+                  {breadcrumb.metadata?.name ??
+                    breadcrumb.id?.path?.at(-1) ??
+                    '?'}
+                </Button>
+                {idx === breadcrumbs.length - 1 ? null : (
+                  <SizableText size="xs" color="muted">
+                    /
+                  </SizableText>
+                )}
+              </React.Fragment>
+            ))}
+          </div>
+
+          <SizableText
+            weight="bold"
+            className="block w-full truncate overflow-hidden text-left whitespace-nowrap"
+          >
+            {getMetadataName(metadata)}
+          </SizableText>
         </div>
-        <SizableText
-          weight="bold"
-          className="block w-full flex-1 truncate overflow-hidden text-left whitespace-nowrap"
-        >
-          {getMetadataName(metadata)}
-        </SizableText>
+
+        <div className="flex flex-shrink-0 items-center gap-2">
+          <SizableText size="xs" color="muted">
+            {formattedDateMedium(new Date(item.lastUpdateTime))}
+          </SizableText>
+
+          <Button
+            variant="destructive"
+            className="hover:bg-destructive/75 dark:hover:bg-destructive/75 cursor-pointer opacity-0 group-hover:opacity-100"
+            size="icon"
+            onClick={(e) => {
+              e.stopPropagation()
+              deleteDialog.open({
+                draftId: item.id,
+                // TODO: add toast?
+                onSuccess: () => {},
+              })
+            }}
+          >
+            <Trash className="size-3" />
+          </Button>
+        </div>
       </div>
-      <SizableText
-        size="xs"
-        color="muted"
-        className="flex-shrink-0 flex-grow-0"
-      >
-        {formattedDateMedium(new Date(item.lastUpdateTime))}
-      </SizableText>
-    </Button>
+
+      <div onClick={(e) => e.stopPropagation()}>{deleteDialog.content}</div>
+    </div>
   )
 }
