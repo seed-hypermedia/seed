24a11185b55eef081679ce54fc7e47cfc8f240da
Horacio Herrera <hi@horacioh.com>
2025-08-22 17:48:53 +0200

    firx embedView wrong html prop

--- frontend/apps/desktop/src/components/app-embeds.tsx
+++ frontend/apps/desktop/src/components/app-embeds.tsx
@@ -56,6 +56,7 @@ function EmbedWrapper({
   viewType = 'Content',
   hideBorder = false,
   isRange = false,
+  embedView,
   ...props
 }: PropsWithChildren<
   {
@@ -65,6 +66,7 @@ function EmbedWrapper({
     viewType?: 'Content' | 'Card'
     hideBorder?: boolean
     isRange?: boolean
+    embedView?: any
   } & Omit<HTMLAttributes<HTMLDivElement>, 'id'>
 >) {
   const docContentContext = useDocContentContext()
