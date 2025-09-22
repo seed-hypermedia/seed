a185249f53a6a2a4b12cfd6d3cc34bafd2e98b94
Alexandr Burdiyan <burdiyan@gmail.com>
2025-09-23 00:46:27 +0200

    feat(frontend/web): follow account redirects fully
    Previously we were only following one account redirect.
    This commit implements recursive redirect following until a hard-coded limit is reached or the redirect chain is resolved,
    failing if cycles are detected.

--- frontend/apps/web/app/routes/hm.api.account.$.tsx
+++ frontend/apps/web/app/routes/hm.api.account.$.tsx
@@ -4,12 +4,52 @@ import {wrapJSON, WrappedResponse} from '@/wrapping'
 import {toPlainMessage} from '@bufbuild/protobuf'
 import {ConnectError, Code} from '@connectrpc/connect'
 import {Params} from '@remix-run/react'
-import {HMDocumentMetadataSchema, hmId, HMMetadataPayload} from '@shm/shared'
+import {
+  Account,
+  HMDocumentMetadataSchema,
+  hmId,
+  HMMetadataPayload,
+} from '@shm/shared'
 
 async function getAccount(accountUid: string) {
-  const grpcAccount = await grpcClient.documents.getAccount({
-    id: accountUid,
-  })
+  // We follow account redirects here until we reach the final account,
+  // or the limit of redirects is reached.
+
+  // Arbitrary limit to prevent absurdly long account redirect chains.
+  const maxAccountRedirects = 100
+
+  const visited = new Set<string>()
+
+  // Final account after following redirects.
+  let grpcAccount: Account | undefined
+  for (let i = 0; i <= maxAccountRedirects; i++) {
+    grpcAccount = await grpcClient.documents.getAccount({
+      id: accountUid,
+    })
+
+    if (!grpcAccount.aliasAccount) {
+      break
+    }
+
+    if (visited.has(grpcAccount.aliasAccount)) {
+      throw new Error(`Account redirect cycle detected`)
+    }
+
+    if (i === maxAccountRedirects) {
+      throw new Error(
+        `Account redirect chain is too long (${maxAccountRedirects})`,
+      )
+    }
+
+    visited.add(grpcAccount.aliasAccount)
+    accountUid = grpcAccount.aliasAccount
+  }
+
+  // This should never happen, because gRPC error would have been thrown before,
+  // but TS compile is unhappy without this check.
+  if (!grpcAccount) {
+    throw new Error(`Unreachable: Account not found`)
+  }
 
   const serverAccount = toPlainMessage(grpcAccount)
   if (serverAccount.aliasAccount) {
