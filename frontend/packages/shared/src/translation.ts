2fedbfa9844dbb6de5d7461889784a2f592628f0
Alexandr Burdiyan <burdiyan@gmail.com>
2025-10-05 22:14:39 +0200

    fix(frontend): memoize useTx to avoid re-renders
    The link keys toast on the web was re-rendering and flickering more than necessary because it's using the value of useTx in its deps,
    and it wasn't memoized.
    
    This commit makes useTx and useTxString wrap their return values in useCallback.

--- frontend/packages/shared/src/translation.ts
+++ frontend/packages/shared/src/translation.ts
@@ -1,4 +1,4 @@
-import {ReactNode} from 'react'
+import {ReactNode, useCallback} from 'react'
 import {useUniversalAppContext} from './routing'
 import {
   AnyTimestamp,
@@ -18,48 +18,54 @@ export type LanguagePack = {
 
 export function useTx() {
   const {languagePack} = useUniversalAppContext()
-  return <TranslationArgs>(
-    enTextOrKey: string,
-    enTextOrGetText?: string | ((args: TranslationArgs) => ReactNode),
-    args?: TranslationArgs,
-  ): ReactNode => {
-    const def =
-      languagePack?.translations?.[enTextOrKey] ??
-      enTextOrGetText ??
-      enTextOrKey
-    if (def && typeof def === 'function') {
-      if (!args) {
-        throw new Error(
-          'args are required when using a function as a translation',
-        )
+  return useCallback(
+    <TranslationArgs>(
+      enTextOrKey: string,
+      enTextOrGetText?: string | ((args: TranslationArgs) => ReactNode),
+      args?: TranslationArgs,
+    ): ReactNode => {
+      const def =
+        languagePack?.translations?.[enTextOrKey] ??
+        enTextOrGetText ??
+        enTextOrKey
+      if (def && typeof def === 'function') {
+        if (!args) {
+          throw new Error(
+            'args are required when using a function as a translation',
+          )
+        }
+        return (def as any)(args)
       }
-      return (def as any)(args)
-    }
-    return def as any
-  }
+      return def as any
+    },
+    [],
+  )
 }
 
 export function useTxString() {
   const {languagePack} = useUniversalAppContext()
-  return <TranslationArgs>(
-    enTextOrKey: string,
-    enTextOrGetText?: string | ((args: TranslationArgs) => string),
-    args?: TranslationArgs,
-  ): string => {
-    const def =
-      languagePack?.translations?.[enTextOrKey] ??
-      enTextOrGetText ??
-      enTextOrKey
-    if (def && typeof def === 'function') {
-      if (!args) {
-        throw new Error(
-          'args are required when using a function as a translation',
-        )
+  return useCallback(
+    <TranslationArgs>(
+      enTextOrKey: string,
+      enTextOrGetText?: string | ((args: TranslationArgs) => string),
+      args?: TranslationArgs,
+    ): string => {
+      const def =
+        languagePack?.translations?.[enTextOrKey] ??
+        enTextOrGetText ??
+        enTextOrKey
+      if (def && typeof def === 'function') {
+        if (!args) {
+          throw new Error(
+            'args are required when using a function as a translation',
+          )
+        }
+        return (def as any)(args)
       }
-      return (def as any)(args)
-    }
-    return def as any
-  }
+      return def as any
+    },
+    [],
+  )
 }
 
 export function useTxUtils() {
