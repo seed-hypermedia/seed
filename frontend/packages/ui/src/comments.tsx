300b1f7264b411e956d4e057a8536229a72e7ce8
Eric Vicenti <eric@vicenti.net>
2025-10-06 08:09:08 -0700

    Formatting fix.. why my cursor has no respect?!

--- frontend/packages/ui/src/comments.tsx
+++ frontend/packages/ui/src/comments.tsx
@@ -103,9 +103,9 @@ export function CommentDiscussions({
           <EmptyDiscussions emptyReplies />
         )
       ) : null}
-      <div className="relative py-4 border-b border-border max-h-1/2">
+      <div className="border-border relative max-h-1/2 border-b py-4">
         <div
-          className="absolute w-px bg-border"
+          className="bg-border absolute w-px"
           style={{
             height: 40,
             top: -16,
@@ -118,7 +118,7 @@ export function CommentDiscussions({
       {commentGroupReplies.data?.length > 0 ? (
         commentGroupReplies.data.map((cg) => {
           return (
-            <div key={cg.id} className={cn('p-2 border-b border-border')}>
+            <div key={cg.id} className={cn('border-border border-b p-2')}>
               <CommentGroup
                 key={cg.id}
                 commentGroup={cg}
@@ -162,7 +162,7 @@ export function Discussions({
 
   if (discussionsService.isLoading && !discussionsService.data) {
     panelContent = (
-      <div className="flex justify-center items-center">
+      <div className="flex items-center justify-center">
         <Spinner />
       </div>
     )
@@ -172,7 +172,7 @@ export function Discussions({
         <>
           {discussionsService.data.discussions?.map((cg) => {
             return (
-              <div key={cg.id} className={cn('border-b border-border')}>
+              <div key={cg.id} className={cn('border-border border-b')}>
                 <CommentGroup
                   commentGroup={cg}
                   authors={discussionsService.data.authors}
@@ -187,7 +187,7 @@ export function Discussions({
           })}
           {discussionsService.data.citingDiscussions?.map((cg) => {
             return (
-              <div key={cg.id} className={cn('border-b border-border')}>
+              <div key={cg.id} className={cn('border-border border-b')}>
                 <CommentGroup
                   commentGroup={cg}
                   authors={discussionsService.data.authors}
@@ -246,7 +246,7 @@ export function BlockDiscussions({
     )
   } else if (doc.isInitialLoading) {
     quotedContent = (
-      <div className="flex justify-center items-center">
+      <div className="flex items-center justify-center">
         <Spinner />
       </div>
     )
@@ -261,7 +261,7 @@ export function BlockDiscussions({
       <>
         {commentsService.data.comments.map((comment) => {
           return (
-            <div key={comment.id} className={cn('p-2 border-b border-border')}>
+            <div key={comment.id} className={cn('border-border border-b p-2')}>
               <Comment
                 isFirst
                 isLast
@@ -287,7 +287,7 @@ export function BlockDiscussions({
       <AccessoryBackButton onClick={onBack} />
       {quotedContent}
       <div className="px-2 pr-4">{commentEditor}</div>
-      <div className="pt-2 mt-2 border-t border-border">{panelContent}</div>
+      <div className="border-border mt-2 border-t pt-2">{panelContent}</div>
     </AccessoryContent>
   )
 }
@@ -316,7 +316,7 @@ export function CommentGroup({
   const firstComment = commentGroup.comments[0]
 
   return (
-    <div className="flex relative flex-col gap-2 p-2">
+    <div className="relative flex flex-col gap-2 p-2">
       {/* {commentGroup.comments.length > 1 && (
         <div
           className="absolute w-px bg-border"
@@ -476,10 +476,10 @@ export function Comment({
           ) : null}
           <div
             className={cn(
-              'absolute top-0 left-0 bg-transparent rounded-full transition-all duration-200 ease-in-out z-2 size-5',
+              'absolute top-0 left-0 z-2 size-5 rounded-full bg-transparent transition-all duration-200 ease-in-out',
               highlight
                 ? 'outline-secondary hover:outline-secondary'
-                : 'outline-white dark:outline-background dark:hover:outline-background hover:outline-white',
+                : 'dark:outline-background dark:hover:outline-background outline-white hover:outline-white',
             )}
             {...authorLink}
           />
@@ -493,20 +493,20 @@ export function Comment({
               />
             </div>
           )}
-          {!isLast ? <div className="w-px h-full bg-border" /> : null}
+          {!isLast ? <div className="bg-border h-full w-px" /> : null}
         </div>
       )}
 
-      <div className="flex flex-col flex-1 gap-1 w-full">
+      <div className="flex w-full flex-1 flex-col gap-1">
         {heading ? (
           <div className="inline">{heading}</div>
         ) : (
-          <div className="flex overflow-hidden gap-2 justify-between items-center pr-2 group">
+          <div className="group flex items-center justify-between gap-2 overflow-hidden pr-2">
             {heading ? null : (
-              <div className="flex overflow-hidden gap-1 items-baseline">
+              <div className="flex items-baseline gap-1 overflow-hidden">
                 <button
                   className={cn(
-                    'px-1 h-5 text-sm font-bold truncate rounded transition-colors hover:bg-accent',
+                    'hover:bg-accent h-5 truncate rounded px-1 text-sm font-bold transition-colors',
                     authorLink ? 'cursor-pointer' : '',
                   )}
                   {...authorLink}
@@ -515,10 +515,10 @@ export function Comment({
                 </button>
                 {externalTarget ? (
                   <>
-                    <span className="text-xs text-muted-foreground">on</span>
+                    <span className="text-muted-foreground text-xs">on</span>
                     <button
                       {...externalTargetLink}
-                      className="px-1 h-5 text-sm font-bold truncate rounded transition-colors hover:bg-accent"
+                      className="hover:bg-accent h-5 truncate rounded px-1 text-sm font-bold transition-colors"
                     >
                       {externalTarget.metadata?.name}
                     </button>
@@ -527,12 +527,12 @@ export function Comment({
                 <CommentDate comment={comment} />
               </div>
             )}
-            <div className="flex gap-2 items-center">
+            <div className="flex items-center gap-2">
               <Tooltip content={tx('Copy Comment Link')}>
                 <Button
                   size="iconSm"
                   variant="ghost"
-                  className="transition-opacity duration-200 ease-in-out text-muted-foreground group-hover:opacity-100 sm:opacity-0"
+                  className="text-muted-foreground transition-opacity duration-200 ease-in-out group-hover:opacity-100 sm:opacity-0"
                   onClick={() => {
                     const url = getUrl(hmId(comment.id))
                     copyTextToClipboard(url)
@@ -620,7 +620,7 @@ function CommentDate({comment}: {comment: HMComment}) {
   )
   return (
     <a
-      className="text-xs underline truncate rounded text-muted-foreground hover:text-muted-foreground"
+      className="text-muted-foreground hover:text-muted-foreground truncate rounded text-xs underline"
       {...link}
     >
       {formattedDateShort(comment.createTime)}
@@ -642,8 +642,8 @@ export function QuotedDocBlock({
   }, [doc.content, blockId])
 
   return (
-    <div className="rounded-lg bg-brand-50 dark:bg-brand-950">
-      <div className="flex relative gap-1 p-2 rounded-lg transition-all duration-200 ease-in-out">
+    <div className="bg-brand-50 dark:bg-brand-950 rounded-lg">
+      <div className="relative flex gap-1 rounded-lg p-2 transition-all duration-200 ease-in-out">
         <div className="flex-shrink-0 py-1.5">
           <BlockQuote size={23} />
         </div>
@@ -701,8 +701,8 @@ export function EmptyDiscussions({
 }) {
   const tx = useTxString()
   return (
-    <div className="flex flex-col gap-4 items-center py-4">
-      <MessageSquare className="text-gray-200 size-25" size={48} />
+    <div className="flex flex-col items-center gap-4 py-4">
+      <MessageSquare className="size-25 text-gray-200" size={48} />
       <SizableText size="md">
         {tx(emptyReplies ? 'Be the first on replying' : 'No discussions')}
       </SizableText>
