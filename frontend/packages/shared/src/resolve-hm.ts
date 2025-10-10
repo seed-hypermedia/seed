5eb7bf9164276715c0f993fe0572bf73da27bea5
Eric Vicenti <eric@vicenti.net>
2025-10-10 01:17:37 -0700

    HM resolver use URL-decoded title to handle special chars

--- frontend/packages/shared/src/resolve-hm.ts
+++ frontend/packages/shared/src/resolve-hm.ts
@@ -5,7 +5,9 @@ export async function resolveHypermediaUrl(url: string) {
   if (response.status === 200) {
     const id = response.headers.get('x-hypermedia-id')
     const version = response.headers.get('x-hypermedia-version')
-    const title = response.headers.get('x-hypermedia-title')
+    const encodedTitle = response.headers.get('x-hypermedia-title')
+    const title = encodedTitle ? decodeURIComponent(encodedTitle) : null
+
     if (id && version) {
       return {id, version, title}
     }
