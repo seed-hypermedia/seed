90fa6fe01d0dd2a1dc2d217d4b21992ab184fa4e
Eric Vicenti <eric@vicenti.net>
2025-11-07 14:26:55 +0100

    fmt

--- frontend/apps/desktop/src/pages/document-content-provider.tsx
+++ frontend/apps/desktop/src/pages/document-content-provider.tsx
@@ -71,7 +71,10 @@ export function AppDocContentProvider({
                 if (blockId && reference && shouldCopy) {
                   reference.onCopy(blockId, blockRange || {expanded: true})
                 }
-                if (route.key === 'document' && blockRange?.copyToClipboard !== true) {
+                if (
+                  route.key === 'document' &&
+                  blockRange?.copyToClipboard !== true
+                ) {
                   const element = window.document.getElementById(blockId)
                   if (element) {
                     element.scrollIntoView({behavior: 'smooth', block: 'start'})
