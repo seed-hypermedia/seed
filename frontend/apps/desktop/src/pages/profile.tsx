14cd9b068fce389f746751b0f43f7e89a4416409
Eric Vicenti <eric@vicenti.net>
2025-11-12 15:33:26 +0100

    Desktop to handle profile redirects

--- frontend/apps/desktop/src/pages/profile.tsx
+++ frontend/apps/desktop/src/pages/profile.tsx
@@ -1,12 +1,14 @@
 import {MainWrapper} from '@/components/main-wrapper'
 import {useSubscribedResource} from '@/models/entities'
 import {useSelectedAccount} from '@/selected-account'
+import {useNavigate} from '@/utils/useNavigate'
 import {ActivityProvider} from '@shm/shared/activity-service-provider'
-import {useResolvedResource} from '@shm/shared/models/entity'
+import {useAccount} from '@shm/shared/models/entity'
 import {useNavRoute} from '@shm/shared/utils/navigation'
 import {PanelContainer} from '@shm/ui/container'
 import {HMProfilePage} from '@shm/ui/profile-page'
-import {useMemo} from 'react'
+import {toast} from '@shm/ui/toast'
+import {useEffect, useMemo} from 'react'
 import {DesktopActivityService} from '../desktop-activity-service'
 import {AppBlocksContentProvider} from './document-content-provider'
 
@@ -14,12 +16,22 @@ export default function ProfilePage() {
   const route = useNavRoute()
   const profileRoute = route.key === 'profile' ? route : null
   const profileId = profileRoute!.id
-  const profile = useResolvedResource(profileId)
-  const doc = profile.data?.type === 'document' ? profile.data.document : null
+  const profile = useAccount(profileId.uid)
   const activityService = useMemo(() => new DesktopActivityService(), [])
   const selectedAccount = useSelectedAccount()
-
-  useSubscribedResource(profileId)
+  const navigate = useNavigate('replace')
+  const redirectDestination =
+    profile.data?.id.uid && profile.data.id.uid !== profileId.uid
+      ? profile.data.id
+      : null
+  useEffect(() => {
+    // todo, make this actually make sense.
+    if (redirectDestination) {
+      toast('This account redirects to another account.')
+      navigate({key: 'profile', id: redirectDestination})
+    }
+  }, [redirectDestination])
+  useSubscribedResource(profileId, true)
 
   return (
     <PanelContainer>
@@ -31,11 +43,13 @@ export default function ProfilePage() {
             textUnit={16}
             layoutUnit={18}
           >
-            <HMProfilePage
-              profile={{id: profileId, metadata: doc?.metadata ?? null}}
-              currentAccount={selectedAccount?.id.uid ?? ''}
-              onEditProfile={null}
-            />
+            {profile.data && (
+              <HMProfilePage
+                profile={profile.data}
+                currentAccount={selectedAccount?.id.uid ?? ''}
+                onEditProfile={null}
+              />
+            )}
           </AppBlocksContentProvider>
         </ActivityProvider>
       </MainWrapper>
