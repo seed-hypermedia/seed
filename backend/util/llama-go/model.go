df3175e2c88cf189eabbf799ffe8abda90ef6bf0
juligasa <11684004+juligasa@users.noreply.github.com>
2026-02-04 22:52:12 +0100

    fix(daemon): not using omp in macos

--- backend/util/llama-go/model.go
+++ backend/util/llama-go/model.go
@@ -12,7 +12,6 @@ import (
 #cgo CPPFLAGS: -I./llama.cpp -I./ -I./llama.cpp/ggml/include -I./llama.cpp/include -I./llama.cpp/common -I./llama.cpp/vendor
 #cgo LDFLAGS: -L./ -lbinding -lcommon -lllama -lggml -lggml-cpu -lggml-base -lstdc++ -lm
 #cgo linux LDFLAGS: -lgomp
-#cgo darwin LDFLAGS: -lomp
 #include "wrapper.h"
 #include <stdlib.h>
 
