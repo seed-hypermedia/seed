1968afeb37fc53ceb506a34f19c4b627c56dd222
juligasa <11684004+juligasa@users.noreply.github.com>
2026-02-06 01:43:29 +0100

    wip(ci): attempt to fix macos/win

--- backend/util/llama-go/model.go
+++ backend/util/llama-go/model.go
@@ -10,9 +10,11 @@ import (
 /*
 #cgo CFLAGS: -I./llama.cpp -I./ -I./llama.cpp/ggml/include -I./llama.cpp/include -I./llama.cpp/common -I./llama.cpp/vendor
 #cgo CPPFLAGS: -I./llama.cpp -I./ -I./llama.cpp/ggml/include -I./llama.cpp/include -I./llama.cpp/common -I./llama.cpp/vendor
+#cgo CXXFLAGS: -std=c++17 -I./llama.cpp -I./ -I./llama.cpp/ggml/include -I./llama.cpp/include -I./llama.cpp/common -I./llama.cpp/vendor
+#cgo darwin CXXFLAGS: -stdlib=libc++
 #cgo LDFLAGS: -L./ -lbinding -lcommon -lllama -lggml -lggml-cpu -lggml-base -lstdc++ -lm
 #cgo linux LDFLAGS: -lgomp
-#cgo darwin LDFLAGS: -framework Accelerate
+#cgo darwin LDFLAGS: -framework Accelerate -stdlib=libc++
 #include "wrapper.h"
 #include <stdlib.h>
 
