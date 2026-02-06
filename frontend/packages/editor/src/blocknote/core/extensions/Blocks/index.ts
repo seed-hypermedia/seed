cd5d0a650be5f1c2ca622483d13942495d40dfdd
Iskak Toltay <iskaktoltay@gmail.com>
2026-02-06 12:44:50 +0500

    feat(editor): Add new dedicated node types for lists. - create ListGroup node for <ul><ol> mapping - create ListContainer node for <li> mapping - add styles for new node types - update nodeConversions to handle new node types

--- frontend/packages/editor/src/blocknote/core/extensions/Blocks/index.ts
+++ frontend/packages/editor/src/blocknote/core/extensions/Blocks/index.ts
@@ -1,6 +1,8 @@
 import {Node} from '@tiptap/core'
 export {BlockContainer} from './nodes/BlockContainer'
 export {BlockGroup} from './nodes/BlockGroup'
+export {ListContainer} from './nodes/ListContainer'
+export {ListGroup} from './nodes/ListGroup'
 export const Doc = Node.create({
   name: 'doc',
   topNode: true,
