cfa0b8ed8dae48aa0444c03245bcca74003f0958
Iskak Toltay <iskaktoltay@gmail.com>
2026-03-13 12:33:30 +0500

    feat(editor): fix grid layout styling in editor content. add responsive stying and rendering for grid layout in published content.
    - Add editor CSS for grid cells: no indentation, media containment, responsive breakpoints
    - Add Grid layout to published content with responsive tailwind grid classes

--- frontend/packages/client/src/hm-types.ts
+++ frontend/packages/client/src/hm-types.ts
@@ -148,6 +148,7 @@ const textBlockProperties = {
 
 const parentBlockAttributes = {
   childrenType: HMBlockChildrenTypeSchema.optional(),
+  columnCount: z.number().optional(),
 }
 
 export const HMBlockParagraphSchema = z
